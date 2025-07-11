84f40eb27b43af5742615b23375ce6af7e96758a
Kevin_KyawThiha <kyaw.thiha@mail.utoronto.ca>
2025-07-11 15:47:58 -0400

    Added code for preprocessing the data

---
+++ src/change_key.py
@@ -0,0 +1,42 @@
+from scipy.io import loadmat, savemat
+import os
+import numpy as np
+
+FILE_PATH = "../data"
+
+
+def change_key(input_dir: str, output_dir: str, key: str):
+    """
+    A function that change the key of mat files from the `input_dir`,
+    and save the clean versions in the `output_dir`
+    Currently, it does
+    Change the main key to be 'key' parameter
+
+    Note that
+    - X should have 'msi' key
+    - Y should have 'RGB' key
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    for fname in os.listdir(input_dir):
+        if not fname.endswith(".mat"):
+            continue
+        data = loadmat(os.path.join(input_dir, fname))
+
+        for key, value in data.items():
+            if key.startswith("__"):
+                continue
+            if isinstance(value, np.ndarray):
+                img = data.get(key)
+                savemat(
+                    os.path.join(output_dir, fname),
+                    {key: img},
+                )
+        print(f"[✓] Key Changed: {fname} to {key}")
+
+
+if __name__ == "__main__":
+    print(f"Changing the keys of the files from {FILE_PATH}")
+    change_key(f"{FILE_PATH}/raw", f"{FILE_PATH}/test/X", "msi")
+    change_key(f"{FILE_PATH}/raw", f"{FILE_PATH}/test/Y", "RGB")
+    print("-------------------------------------")
---
+++ src/downsample.py
@@ -0,0 +1,97 @@
+from scipy.io import loadmat, savemat
+import cv2
+import numpy as np
+from sklearn.decomposition import PCA
+import os
+
+FILE_PATH = "../data"
+
+
+def load_downsample_save(
+    input_dir: str,
+    output_dir: str,
+    key: str,
+    spatial_scale=4,
+    spectral_scale=4,
+    spectral_algorithm="uniform",
+):
+    """
+    A function that
+    1. loads the mat image from input_dir,
+    2. downsample the given image by given spatial & spectral scales
+    3. save the image in .mat form in output_dir
+
+    Parameters:
+    - input_dir: Input directory to load a list of mat images
+    - output_dir: Output directory to save the downsampled mat images
+    - key: key to access the img from mat files (msi for X, and RGB for Y)
+    - img: h x w x s image file
+    - spatial_scale: how much scale to downsample on spatial level
+    - spectral_scale: how much scale to downsample on spectral level
+    - spectral_algorithm: what algorithm to use for spectral downsampling
+      "uniform" - Sample every 'spectral_scale' time
+      "pca" - Sample using Principal Component Analysis (Retains spectral bands with most effect on data)
+
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    for fname in os.listdir(input_dir):
+        if not fname.endswith(".mat"):
+            continue
+        data = loadmat(os.path.join(input_dir, fname))
+        img = data.get(key)
+        img = downsample(img, spatial_scale, spectral_scale, spectral_algorithm)
+
+        savemat(
+            os.path.join(output_dir, fname),
+            {key: img},
+        )
+
+
+def downsample(img, spatial_scale=4, spectral_scale=4, spectral_algorithm="uniform"):
+    """
+    A function that downsample the given image by given spatial & spectral scales
+    Parameters:
+    - img: h x w x s image file
+    - spatial_scale: how much scale to downsample on spatial level
+    - spectral_scale: how much scale to downsample on spectral level
+    - spectral_algorithm: what algorithm to use for spectral downsampling
+      "uniform" - Sample every 'spectral_scale' time
+      "pca" - Sample using Principal Component Analysis (Retains spectral bands with most effect on data)
+    """
+    if spectral_algorithm != "uniform" and spectral_algorithm != "pca":
+        return
+
+    h, w, c = img.shape
+
+    # Downsample spatially
+    lowres = np.zeros((h // spatial_scale, w // spatial_scale, c), dtype=np.float32)
+    for i in range(c):
+        band = img[:, :, i]
+        band = cv2.resize(
+            band,
+            (w // spatial_scale, h // spatial_scale),
+            interpolation=cv2.INTER_CUBIC,
+        )
+        lowres[:, :, i] = band
+
+    # Downsample spectrally
+    if spectral_algorithm == "uniform":
+        # 1. Uniformly sample every spectral_scale-th band
+        lowres = lowres[:, :, ::spectral_scale]
+    elif spectral_algorithm == "pca":
+        # 2. PCA-based spectral downsampling
+        out_bands = c // spectral_scale
+        H, W, C = lowres.shape
+        reshaped = lowres.reshape(-1, C)  # shape: (H*W, C)
+        pca = PCA(n_components=out_bands)
+        projected = pca.fit_transform(reshaped)  # shape: (H*W, out_bands)
+        lowres = projected.reshape(H, W, out_bands)
+
+    return lowres
+
+
+if __name__ == "__main__":
+    print(f"Downsampling the files from {FILE_PATH}")
+    load_downsample_save(f"{FILE_PATH}/test/X", f"{FILE_PATH}/test/X", "msi")
+    print("-------------------------------------")
